0c09e781b56b99ca15f81bd1dc5be8d58e6a5fc6
Peter Zsilak <peter.zsilak@gmail.com>
2018-06-04 01:20:01 +0200

    Basic Spring app generated.

---
+++ src/main/java/me/tvstore/watcher/WatcherApplication.java
@@ -0,0 +1,12 @@
+package me.tvstore.watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatcherApplication.class, args);
+    }
+}
